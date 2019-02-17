7c7e204dce606067f18be3a1836ba4f95567c440
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-02-17 18:53:15 -0500

    Camera

---
+++ Assets/CameraSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpawn : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject guard;
+    public GameObject cinematicCamera;
+    public float time = 7;
+  
+    private void Update ()
+    {
+        if ((Time.time >= time))
+        {
+            player.SetActive(true);
+            guard.SetActive(true);
+            cinematicCamera.SetActive(false);
+        }
+    }
+}
