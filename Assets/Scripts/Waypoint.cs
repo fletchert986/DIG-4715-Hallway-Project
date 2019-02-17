4dbe4c5c3cd6f113d958f3e4b68a66eb01812340
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-02-16 22:56:32 -0500

    Colliders and Waypoints

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    float debugDrawRadius = 1.0F;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius);
+    }
+}
