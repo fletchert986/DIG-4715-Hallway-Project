a9dd34ef817f81096c6d601df987568a09e5781b
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-02-18 21:15:38 -0500

    Cinematic Messages

--- Assets/Scripts/CameraSpawn.cs
+++ Assets/Scripts/CameraSpawn.cs
@@ -7,7 +7,10 @@ public class CameraSpawn : MonoBehaviour
     public GameObject player;
     public GameObject guard;
     public GameObject cinematicCamera;
+    public GameObject step2;
+    public GameObject step1;
     public float time;
+    public float step2time;
     private float startTime = 0;
 
 
@@ -22,8 +25,16 @@ public class CameraSpawn : MonoBehaviour
 
     private void Update ()
     {
+        if ((Time.time >= startTime + step2time))
+        {
+            step1.SetActive(false);
+            step2.SetActive(true);
+        }
+
+
         if ((Time.time >= startTime + time))
         {
+            step2.SetActive(false);
             player.SetActive(true);
             guard.SetActive(true);
             cinematicCamera.SetActive(false);
